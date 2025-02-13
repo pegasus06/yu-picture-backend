72ccfce14760ede73d66e4585ee65aee14cf6bf5
ryan <a88483509@gmail.com>
2025-02-13 16:55:40 +0800

    123

---
+++ src/main/java/com/ruizhou/picturebackend/service/UserService.java
@@ -0,0 +1,66 @@
+package com.ruizhou.picturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.ruizhou.picturebackend.model.dto.user.UserQueryRequest;
+import com.ruizhou.picturebackend.model.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ruizhou.picturebackend.model.vo.LoginUserVO;
+import com.ruizhou.picturebackend.model.vo.UserVO;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * @author RZ
+ * @description 针对表【user(用户)】的数据库操作Service
+ * @createDate 2025-02-12 13:43:24
+ */
+public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    String getEncryptPassword(String userPassword);
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    UserVO getUserVO(User user);
+
+    List<UserVO> getUserVOList(List<User> userList);
+
+    QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+}
