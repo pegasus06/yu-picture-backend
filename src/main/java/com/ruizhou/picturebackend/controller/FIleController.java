fa02c7e34aa215b2aa18794ccbf402f395800099
ryan <a88483509@gmail.com>
2025-02-17 16:47:03 +0800

    更新上传逻辑

--- src/main/java/com/ruizhou/picturebackend/controller/FIleController.java
+++ src/main/java/com/ruizhou/picturebackend/controller/FIleController.java
@@ -1,5 +1,8 @@
 package com.ruizhou.picturebackend.controller;
 
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.COSObjectInputStream;
+import com.qcloud.cos.utils.IOUtils;
 import com.ruizhou.picturebackend.annotation.AuthCheck;
 import com.ruizhou.picturebackend.common.BaseResponse;
 import com.ruizhou.picturebackend.common.ResultUtils;
@@ -9,13 +12,16 @@ import com.ruizhou.picturebackend.manager.CosManager;
 import com.ruizhou.picturebackend.model.constant.UserConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -29,7 +35,7 @@ public class FIleController {
      * @param multipartFile
      * @return
      */
-//    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     @PostMapping("/test/upload")
     public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
         // 文件目录
@@ -56,5 +62,36 @@ public class FIleController {
             }
         }
     }
+    /**
+     * 测试文件下载
+     *
+     * @param filepath 文件路径
+     * @param response 响应对象
+     */
+//    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @GetMapping("/test/download/")
+    public void testDownloadFile(String filepath, HttpServletResponse response) throws IOException {
+        COSObjectInputStream cosObjectInput = null;
+        try {
+            COSObject cosObject = cosManager.getObject(filepath);
+            cosObjectInput = cosObject.getObjectContent();
+            // 处理下载到的流
+            byte[] bytes = IOUtils.toByteArray(cosObjectInput);
+            // 设置响应头
+            response.setContentType("application/octet-stream;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + filepath);
+            // 写入响应
+            response.getOutputStream().write(bytes);
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            log.error("file download error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "下载失败");
+        } finally {
+            if (cosObjectInput != null) {
+                cosObjectInput.close();
+            }
+        }
+    }
+
 
 }
--- src/main/java/com/ruizhou/picturebackend/manager/CosManager.java
+++ src/main/java/com/ruizhou/picturebackend/manager/CosManager.java
@@ -1,6 +1,8 @@
 package com.ruizhou.picturebackend.manager;
 
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.GetObjectRequest;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import com.ruizhou.picturebackend.config.CosClientConfig;
@@ -27,6 +29,15 @@ public class CosManager {
         PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key,
                 file);
         return cosClient.putObject(putObjectRequest);
+    }
+    /**
+     * 下载对象
+     *
+     * @param key 唯一键
+     */
+    public COSObject getObject(String key) {
+        GetObjectRequest getObjectRequest = new GetObjectRequest(cosClientConfig.getBucket(), key);
+        return cosClient.getObject(getObjectRequest);
     }
 
 }
