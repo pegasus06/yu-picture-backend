16ea498cbfc97d353f3d1a872df738ca5df26a50
zhourui <1010656935@qq.com>
2025-02-25 14:18:30 +0800

    增删改查

---
+++ src/main/java/com/ruizhou/picturebackend/controller/SpaceController.java
@@ -0,0 +1,118 @@
+package com.ruizhou.picturebackend.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ruizhou.picturebackend.annotation.AuthCheck;
+import com.ruizhou.picturebackend.common.BaseResponse;
+import com.ruizhou.picturebackend.common.DeleteRequest;
+import com.ruizhou.picturebackend.common.ResultUtils;
+import com.ruizhou.picturebackend.exception.BusinessException;
+import com.ruizhou.picturebackend.exception.ErrorCode;
+import com.ruizhou.picturebackend.exception.ThrowUtils;
+import com.ruizhou.picturebackend.manager.SpaceUserAuthManager;
+import com.ruizhou.picturebackend.model.constant.UserConstant;
+import com.ruizhou.picturebackend.model.dto.space.SpaceAddRequest;
+import com.ruizhou.picturebackend.model.dto.space.SpaceEditRequest;
+import com.ruizhou.picturebackend.model.dto.space.SpaceQueryRequest;
+import com.ruizhou.picturebackend.model.dto.space.SpaceUpdateRequest;
+import com.ruizhou.picturebackend.model.entity.Space;
+import com.ruizhou.picturebackend.model.entity.User;
+import com.ruizhou.picturebackend.model.vo.SpaceVO;
+import com.ruizhou.picturebackend.service.SpaceService;
+import com.ruizhou.picturebackend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/space")
+public class SpaceController {
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private SpaceService spaceService;
+
+    @Resource
+    private SpaceUserAuthManager spaceUserAuthManager;
+
+    @PostMapping("/add")
+    public BaseResponse<Long> addSpace(@RequestBody SpaceAddRequest spaceAddRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf(spaceAddRequest == null, ErrorCode.PARAMS_ERROR);
+        User loginUser = userService.getLoginUser(request);
+        long newId = spaceService.addSpace(spaceAddRequest, loginUser);
+        return ResultUtils.success(newId);
+    }
+
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteSpace(@RequestBody DeleteRequest deleteRequest
+            , HttpServletRequest request) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        Long id = deleteRequest.getId();
+        // 判断是否存在
+        Space oldSpace = spaceService.getById(id);
+        ThrowUtils.throwIf(oldSpace == null, ErrorCode.NOT_FOUND_ERROR);
+        // 仅本人或者管理员可删除
+//        spaceService.checkSpaceAuth(loginUser, oldSpace);
+        // 操作数据库
+        boolean result = spaceService.removeById(id);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
+
+    /**
+     * 更新空间（仅管理员可用）
+     *
+     * @param spaceUpdateRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/update")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> updateSpace(@RequestBody SpaceUpdateRequest spaceUpdateRequest,
+                                             HttpServletRequest request) {
+        if (spaceUpdateRequest == null || spaceUpdateRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 将实体类和 DTO 进行转换
+        Space space = new Space();
+        BeanUtils.copyProperties(spaceUpdateRequest, space);
+        // 自动填充数据
+        spaceService.fillSpaceBySpaceLevel(space);
+        // 数据校验
+        spaceService.validSpace(space, false);
+        // 判断是否存在
+        long id = spaceUpdateRequest.getId();
+        Space oldSpace = spaceService.getById(id);
+        ThrowUtils.throwIf(oldSpace == null, ErrorCode.NOT_FOUND_ERROR);
+        // 操作数据库
+        boolean result = spaceService.updateById(space);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
+
+    /**
+     * 根据 id 获取空间（仅管理员可用）
+     */
+    @GetMapping("/get")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Space> getSpaceById(long id, HttpServletRequest request) {
+        ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        // 查询数据库
+        Space space = spaceService.getById(id);
+        ThrowUtils.throwIf(space == null, ErrorCode.NOT_FOUND_ERROR);
+        // 获取封装类
+        return ResultUtils.success(space);
+    }
+
+
+}
---
+++ src/main/java/com/ruizhou/picturebackend/manager/SpaceUserAuthManager.java
@@ -0,0 +1,4 @@
+package com.ruizhou.picturebackend.manager;
+
+public class SpaceUserAuthManager {
+}
